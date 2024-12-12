ea55c6a6aa287e7bf78b9241b68ba2c02f66e2ec
Liubava <liubava.k@bk.ru>
2024-12-12 23:42:21 +0300

    подправила стили , разметку

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log('zarya example');
+var siteWidth = 1500;
+var scale = screen.width / siteWidth;
+
+document
+  .querySelector('meta[name="viewport"]')
+  .setAttribute(
+    'content',
+    'width=' + siteWidth + ', initial-scale=' + scale + ''
+  );
