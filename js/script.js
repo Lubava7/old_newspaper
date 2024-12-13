a95f776c288e6ad692c19f60958d8aa5c76da52a
Liubava <liubava.k@bk.ru>
2024-12-13 20:11:43 +0300

    подправила стили , разметку , убрала скрипты и добавила зум

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// var siteWidth = 1500;
-// var scale = screen.width / siteWidth;
-
-// document
-//   .querySelector('meta[name="viewport"]')
-//   .setAttribute(
-//     'content',
-//     'width=' + siteWidth + ', initial-scale=' + scale + ''
-//   );
-
 // текущие дата и время
 const dateTimeElement = document.getElementById('date_time');
 const updateDateTime = () => {
