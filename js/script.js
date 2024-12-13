b996a7f9cae3d5c588a85dcaac8ebe53f28486f9
Liubava <liubava.k@bk.ru>
2024-12-13 10:45:53 +0300

    подправила стили , разметку

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ document
     'content',
     'width=' + siteWidth + ', initial-scale=' + scale + ''
   );
+
+// текущие дата и время
+const dateTimeElement = document.getElementById('date_time');
+const updateDateTime = () => {
+  const now = new Date();
+
+  const weekdayOptions = { weekday: 'long' };
+  const dateOptions = { day: 'numeric', month: 'long', year: 'numeric' };
+
+  let weekday = now.toLocaleDateString('ru-RU', weekdayOptions);
+  weekday = weekday.charAt(0).toUpperCase() + weekday.slice(1);
+
+  const fullDate = now.toLocaleDateString('ru-RU', dateOptions);
+
+  dateTimeElement.innerHTML = `
+    ${weekday}</br> № 46 (13626)</br> ${fullDate}
+  `;
+};
+
+updateDateTime();
